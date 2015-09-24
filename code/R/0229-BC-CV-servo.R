24d1eb4ed5620f88dc092b63ff1dd8cbf062d2a4
samuel-rosa <alessandrosamuelrosa@gmail.com>
2015-09-24 10:26:44 +0200

    Addded source code and data from Ruo Xu (2013)

---
+++ code/R/0229-BC-CV-servo.R
@@ -0,0 +1,43 @@
+# Source: Xu, R. Improvements to random forest methodology. Ames, Iowa: Iowa 
+# State University, p. 87, 2013.
+# 
+### Here we tried FOUR kinds of BC methods compared to No-BC to real datasets.
+
+rm(list = ls())
+library(randomForest)
+source("code/R/@iterative-RF-Fast.R")
+
+dat <- read.csv("data/servo.data", header = F ) 
+nsample <- nrow(dat)
+X <- NULL
+for (i in 1:(ncol(dat) - 1)) X <- cbind(X, factor(dat[, i]))
+Y <- dat[, ncol(dat)]
+
+nround <- 1000 # Number of partitioning data to training and test
+numtree <- 500
+ntrain <- round(2 * nsample / 3)
+ntest <- nsample - ntrain
+nz <- 5
+niter <- 10 # Number of iterations
+
+mse <- NULL
+
+k <- 0
+repeat {
+  k <- k + 1
+  id.train <- sample(1:nsample, ntrain, replace = F)
+  id.test <- c(1:nsample)[-id.train]
+  
+  res <- iter.RF(X[id.train,], Y[id.train], X[id.test,], numtree, nsize = nz, niter)
+  res <- res[, -c(1:ntrain)] # prediction for test cases of this holding-out
+  
+  mse <- rbind(mse, apply(((t(res) - Y[id.test])^2), 2, mean))
+  
+  if (k %% 100 == 0) print(k)
+  if (k == nround) break
+  
+} # repeat ends
+
+colnames(mse) <- paste("iter-", 1:niter, sep = "")
+mse <- apply(mse, 2, mean)
+plot(1:niter, mse)
---
+++ code/R/@iterative-RF-Fast.R
@@ -0,0 +1,47 @@
+# Source: Xu, R. Improvements to random forest methodology. Ames, Iowa: Iowa 
+# State University, p. 87, 2013.
+# 
+### bt resamples are drawn separately for every iteration.
+
+### This function grow RFs iteratively.
+### It output a ntest*niter matrix, with each column saving the prediction for 
+### the test case response (or -bias) for an iteration. 
+
+iter.RF <- 
+  function (Xtrain, Ytrain, Xtest, numtree, nsize, niter) {
+    # Xtrain is the design matrix for training cases, n*p; 
+    # Ytrain is the response for training cases, a vector of length n;
+    # Xtest is the design matrix for test cases, m*p;
+    # ntree is the number of trees per forest;
+    # nsize is the maximal node size per tree;
+    # niter is the number of iterations for the bias-correction RFs.
+    
+    ni <- 0
+    ntrain <- nrow(Xtrain)
+    ntest <- nrow(Xtest)
+    pred.iter <- NULL # This records the predicted value for all X's
+    b <- Ytrain # Reponse of training data of each iteration
+    
+    repeat {
+      ni <- ni + 1
+      RF.temp <- randomForest(Xtrain, b, ntree = numtree, nodesize = nsize)
+      bpred.oob <- RF.temp[[3]]
+      bpred.test <- predict(RF.temp, Xtest) # Predict for test cases
+      pred.iter <- rbind(pred.iter, c(bpred.oob, bpred.test))
+      b <- b - bpred.oob
+      if (ni >= niter) break
+    } # repeat ends
+    
+    rownames(pred.iter) <- paste("iter-", 1:niter, sep = "")
+    colnames(pred.iter) <- c(paste("Tr-", 1:ntrain, sep = ""), paste("Test-", 1:ntest, sep = ""))
+    
+    pred <- pred.iter[1,]		# This saves the final prediction results of different iterations of BC
+    for (k in 2:niter){
+      pred = rbind(pred, apply(pred.iter[1:k,], 2, sum))
+    } # for k ends
+    rownames(pred) <- paste("iter-", 1:niter, sep = "")
+    colnames(pred) <- c(paste("Tr-", 1:ntrain, sep = ""), paste("Test-", 1:ntest, sep = ""))
+    
+    return(pred)
+    
+  } # function ends
