7866b83074e131caf438f897be6f1f140c5d2ba5
samuel-rosa <alessandrosamuelrosa@gmail.com>
2015-09-24 13:22:49 +0200

    plot mse profile

--- code/R/0229-BC-CV-servo.R
+++ code/R/0229-BC-CV-servo.R
@@ -7,13 +7,14 @@ rm(list = ls())
 library(randomForest)
 source("code/R/@iterative-RF-Fast.R")
 
-dat <- read.csv("data/servo.data", header = F ) 
+dat <- read.csv("data/servo.data", header = F )
+
 nsample <- nrow(dat)
 X <- NULL
 for (i in 1:(ncol(dat) - 1)) X <- cbind(X, factor(dat[, i]))
 Y <- dat[, ncol(dat)]
 
-nround <- 1000 # Number of partitioning data to training and test
+nround <- 100 # Number of partitioning data to training and test
 numtree <- 500
 ntrain <- round(2 * nsample / 3)
 ntest <- nsample - ntrain
@@ -28,7 +29,7 @@ repeat {
   id.train <- sample(1:nsample, ntrain, replace = F)
   id.test <- c(1:nsample)[-id.train]
   
-  res <- iter.RF(X[id.train,], Y[id.train], X[id.test,], numtree, nsize = nz, niter)
+  res <- iter.RF(X[id.train, ], Y[id.train], X[id.test,], numtree, nsize = nz, niter)
   res <- res[, -c(1:ntrain)] # prediction for test cases of this holding-out
   
   mse <- rbind(mse, apply(((t(res) - Y[id.test])^2), 2, mean))
@@ -38,6 +39,14 @@ repeat {
   
 } # repeat ends
 
+# Plot mse profile
 colnames(mse) <- paste("iter-", 1:niter, sep = "")
+eb <- apply(mse, 2, sd) / sqrt(nround)
 mse <- apply(mse, 2, mean)
-plot(1:niter, mse)
+upper <- (mse + eb) / mse[1]
+lower <- (mse - eb) / mse[1]
+mse <- mse / mse[1]
+plot(1:niter, mse, type = "b", ylim = c(min(lower), max(upper)))
+abline(h = mse[1], col = "red")
+arrows(1:niter, lower, 1:niter, upper, length = 0.05, angle = 90, code = 3)
+which.min(mse)
